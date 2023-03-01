9f2295d087f35632862adb4a92bd3c33434c4524
RubenSiman <rubensimamusic@gmail.com>
2023-03-01 12:26:57 -0600

    Initial Commit

---
+++ GithubProject/GithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubProjectApp.swift
+//  GithubProject
+//
+//  Created by Ruben Siman on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
